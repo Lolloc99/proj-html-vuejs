7aeb8255fb503854372a3424a11e93915a3d42ea
Lolloc99 <lolloc99@yahoo.it>
2022-05-30 12:24:10 +0200

    Init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
